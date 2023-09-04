f511752b08d6b8af9c0fe826f272332e955dcc94
sntgrazi <sntgrazi22@gmail.com>
2023-09-04 20:54:05 -0300

    Adicionando alterações

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "BRIAR",
+    "description": "Briar foi criada pela Rosa Negra, por meio da hemomancia (também conhecida como sangue mágico), para ser uma arma viva. Ela precisa consumir sangue para se recuperar. E se tem uma coisa que ela gosta é de sangue.",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltead8266c9bd9f7c8/64e7a41314f0616acbb3c568/Briar-Splash-abilities-rundown-article.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "MUDANÇA NO CRONOGRAMA DAS RANQUEADAS",
+    "description": "Hoje, anunciamos que o LoL passará a ter três Etapas Ranqueadas a partir do ano que vem. Além disso, dessa vez as grandes mudanças na mecânica de jogo vão acontecer em janeiro, em vez de novembro durante a pré-temporada. ",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt0cfea10deca29f0e/64e7e07cf1ac3dc86df8ff19/2023-Key-Art.jpg"
+  },
+  {
+    "id":"3",
+    "title": "NOVOS BOTS NO PBE (TESTE DE 2 SEMANAS)",
+    "description": "É um prazer ver vocês de novo, amantes do LoLzinho! Aqui quem fala é Darcy “Riot DashiJador” Ludington, e meu colíder do peito, Emmett “Riot Sentanel” Coakley. Estamos aqui para trazer uma atualização sobre o último artigo, /dev: Aprimorando Bots. Sem mais delongas, gostaríamos de anunciar que nossos novos bots de nível introdutório chegarão ao PBE para uma fase de testes em setembro ou outubro e ficarão por duas semanas! ",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt00b4a7f5e35e570d/64e7d81f37cf4671bd13cc82/Nunu_Bot_banner_opt.jpg"
+  },
+  {
+    "id":"4",
+    "title": "VISÃO GERAL DAS MECÂNICAS DE NOVOS HORIZONTES",
+    "description": "Chegou uma frente quente à tempestade da Convergência que reforjou Runeterra! Podemos esperar ventos fortes, a umidade lá no alto e um montão de piratas... yarrr! Neste artigo, vou abordar a maior parte do novo conteúdo de mecânica de jogo, como cada pecinha poderá ser usada logo no lançamento e, como não podia deixar de ser, ostentar meu arsenal de piadinhas. Beleza, bora içar velas! Primeira parada, Águas de Sentina!",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt9914f8ffeacdd687/64e7af0affe8ee4d7f3e7c61/ArticleBanner_1920x1080.jpg"
   }
 ]
